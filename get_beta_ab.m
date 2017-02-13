cbb6c140189746b012ad7b9a4f4e0b2b17ddd61a
Johannes Pfeifer <jpfeifer@gmx.de>
2017-02-13 19:05:28 +0100

    Add missing file get_beta_ab.m

---
+++ get_beta_ab.m
@@ -0,0 +1,30 @@
+function outvalue=get_beta_ab(x,target)
+% function outvalue=get_beta_ab(x,target)
+% provides parameters a and b for beta distribution that matches targets at
+% 0.05, 0.5, and 0.95 quantiles
+%
+% Inputs:
+% - x       [2 by 1] vector with starting values for a and b
+% - target  [3 by 1] vector with targets for quantiles
+% Inputs:
+% - outvalue  [2 by 1] vector with values for a and b (uses the Matlab parameterization)
+%
+% Copyright (C) 2013-2016 Benjamin Born and Johannes Pfeifer
+% 
+%  This is free software: you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation, either version 3 of the License, or
+%  (at your option) any later version.
+% 
+%  It is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+% 
+%  For a copy of the GNU General Public License,
+%  see <http://www.gnu.org/licenses/>.
+
+
+randnumbers=betarnd(x(1),x(2),50000,1); % generate random numbers
+quant_values=quantile(randnumbers,[0.05,0.5,0.95]); %compute quantiles
+outvalue=sum((target-quant_values).^2); %compute sum of squared deviations from target
